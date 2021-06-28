15fdcb91ab536c002ff0d05e066c2937fddc9ad0
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-06-28 17:23:15 -0300

    Refatorado código no index.js e App.js pra usar props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import logo from './logo.svg';
 import React from 'react';
 
-
-const App = () => {
+//props só se usa para leitura
+const App = (props) => {
+  console.log(props)
    return(
      <div>
        <h1>Essa é a nossa App Page</h1>
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//const que usa {} serve pra "desestructurar" a mesma
+const person = {name: "Simone", lastName: "Santos"};
 
 //document.getElementById injeta o HMTL na div root que está na pasta public index.html
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <App name={person.name}  lastName={person.lastName}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
