dcbacb18d9b4f71bce803276ed8bedff60e1325f
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-06-28 18:29:03 -0300

    Criados links para pages Home e About

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const About = () => {
    return(
      <div>
+         <Link to='/'>Voltar para a Home Page</Link>
        <h1>Essa é a nossa About Page</h1>
      </div>
    )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Home = () => {
    return(
      <div>
+     <Link to='/about'>Ir paga a página sobre</Link>
        <h1>Essa é a nossa Home Page</h1>
      </div>
    )
